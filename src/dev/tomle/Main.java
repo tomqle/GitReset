e215be66915a6ff0d6a566a4fcc81ffd15ba7de2
tomqle <tomqle1@gmail.com>
2022-01-15 21:40:00 -0800

    First commit.

---
+++ src/dev/tomle/Main.java
@@ -0,0 +1,9 @@
+package dev.tomle;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("First commit: Hello, world!");
+    }
+}
